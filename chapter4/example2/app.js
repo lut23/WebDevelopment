ac61297be58249025b3c93a670287d4230051a88
Braden Luthi <luthi003@morris.umn.edu>
2014-07-25 00:42:46 -0500

    finished second example in chapter 4
    the java script will now take text from an input box append the text  to
    the page fading it it and then clears the box

--- chapter4/example2/app.js
+++ chapter4/example2/app.js
@@ -1,10 +1,24 @@
 var main = function () {
         "use strict";
+         var addCommentFromInputBox = function() {
+
+             var $new_comment;
+             if($(".comment-input input").val() ? true:false ){
+                $new_comment = $("<p>").text($(".comment-input input").val());
+                $new_comment.hide();
+                $(".comments").append($new_comment);
+                $new_comment.fadeIn();
+                $(".comment-input input").val("");
+             } 
+         };
 	
 	$(".comment-input button").on("click", function (event){
-           	
-                var $new_comment = $("<p>").text($(".comment-input input").val());
-                $(".comments").append($new_comment);
+           addCommentFromInputBox();	
           });
+	$(".comment-input input").on("keypress", function(event){
+		if(event.keyCode === 13){
+			addCommentFromInputBox();
+		}
+	});
 };
 $(document).ready(main);
